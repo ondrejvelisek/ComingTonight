ae32f7e001f8ef2123c274fd8c3e2d8b3b9a8524
xmraz <mikulas.mraz@azet.sk>
2018-05-11 16:03:37 +0200

    Add files via upload

--- app/src/main/java/com/muni/comingtonight/service/ShowRatingServiceOmdb.kt
+++ app/src/main/java/com/muni/comingtonight/service/ShowRatingServiceOmdb.kt
@@ -1,5 +1,10 @@
 package com.muni.comingtonight.service
 
+import com.muni.comingtonight.api.OmdbApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.gildor.coroutines.retrofit.await
+
 /**
  * Implementation based on Omdb API service http://www.omdbapi.com/
  * We registered following api key
@@ -7,8 +12,29 @@ package com.muni.comingtonight.service
  */
 class ShowRatingServiceOmdb : ShowRatingService{
 
-    override suspend fun getRating(title: String): Double {
-        TODO("Unimplemented yet")
+    val omdbApi = Retrofit.Builder()
+            .baseUrl("http://www.omdbapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(OmdbApi::class.java)
+
+    override suspend fun getRating(title: String): Double
+    {
+        val movieInfo = omdbApi.getMovieInfo(title,"74457713"
+        ).await()
+
+        println(movieInfo.toString())
+
+        val  rating = movieInfo
+                .getAsJsonObject("movie")
+                .getAsJsonPrimitive("imdbRating")
+                .asDouble
+
+        if (rating > 0.0)
+            return rating
+        else
+            return 0.0
     }
 
 }
+
