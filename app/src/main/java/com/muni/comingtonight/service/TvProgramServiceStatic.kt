5174b612f7af25a944181bf1276bdc68fc58dc8f
xmraz <mikulas.mraz@azet.sk>
2018-05-16 15:44:14 +0200

    Add files via upload

--- app/src/main/java/com/muni/comingtonight/service/TvProgramServiceStatic.kt
+++ app/src/main/java/com/muni/comingtonight/service/TvProgramServiceStatic.kt
@@ -9,9 +9,9 @@ class TvProgramServiceStatic : TvProgramService {
     private val IMAGE_URI = URI("https://creativescreenwriting.com/wp-content/uploads/2016/10/The-Dark-Knight-Joker-featured.jpg")
 
     private val movies = listOf(
-            Activity("Lord of The ring", 8.9, Category.HOME, null, IMAGE_URI),
-            Activity("Simpsons XXVII.", 8.0, Category.HOME, null, IMAGE_URI),
-            Activity("Game of Thronmes", 7.3, Category.HOME, null, IMAGE_URI)
+            Activity("Taggart", 7.2, Category.HOME, null, URI("https://ia.media-imdb.com/images/M/MV5BMTExODkwMDczODZeQTJeQWpwZ15BbWU3MDI3OTE3MzE@._V1_SX300.jpg")),
+            Activity("Simpsons XXVII.", 6.0, Category.HOME, null, IMAGE_URI),
+            Activity("Game of Thronmes", 7.0, Category.HOME, null, IMAGE_URI)
     )
 
     override suspend fun getTodaysMovies(): List<Activity> {
