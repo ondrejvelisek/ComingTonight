a3153b4c3982407b700874ce4a21aa3020a7651b
xmraz <mikulas.mraz@azet.sk>
2018-06-01 21:28:35 +0200

    Final touches

--- app/src/main/java/com/muni/comingtonight/service/TvProgramServiceStatic.kt
+++ app/src/main/java/com/muni/comingtonight/service/TvProgramServiceStatic.kt
@@ -9,7 +9,7 @@ class TvProgramServiceStatic : TvProgramService {
     private val IMAGE_URI = URI("https://creativescreenwriting.com/wp-content/uploads/2016/10/The-Dark-Knight-Joker-featured.jpg")
 
     private val movies = listOf(
-            Activity("Taggart", 7.2, Category.HOME, null, URI("https://ia.media-imdb.com/images/M/MV5BMTExODkwMDczODZeQTJeQWpwZ15BbWU3MDI3OTE3MzE@._V1_SX300.jpg")),
+            Activity("Dva a půl chlapa", 7.6, Category.HOME, null, URI("https://ia.media-imdb.com/images/M/MV5BOTI2MjIzN2ItZDg0OS00MTlhLWIzMTMtYWI4ZTA0NGE4NDJlXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg")),
             Activity("Simpsons XXVII.", 6.0, Category.HOME, null, IMAGE_URI),
             Activity("Game of Thronmes", 7.0, Category.HOME, null, IMAGE_URI)
     )
